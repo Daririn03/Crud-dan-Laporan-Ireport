e68b87002dbe7a22fde2b16f80ebf9aa5daa5a8a
Daririn03 <46618453+Daririn03@users.noreply.github.com>
2019-01-12 13:41:30 +0700

    Add files via upload

---
+++ Pengolahan Data Pegawai/data pegawai.sql
@@ -0,0 +1,52 @@
+/*
+SQLyog Community v12.09 (64 bit)
+MySQL - 5.6.21 : Database - data_pegawai
+*********************************************************************
+*/
+
+/*!40101 SET NAMES utf8 */;
+
+/*!40101 SET SQL_MODE=''*/;
+
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+CREATE DATABASE /*!32312 IF NOT EXISTS*/`data_pegawai` /*!40100 DEFAULT CHARACTER SET latin1 */;
+
+USE `data_pegawai`;
+
+/*Table structure for table `pegawai` */
+
+DROP TABLE IF EXISTS `pegawai`;
+
+CREATE TABLE `pegawai` (
+  `id` int(11) NOT NULL,
+  `nama` varchar(50) NOT NULL,
+  `alamat` varchar(50) NOT NULL,
+  `telp` varchar(50) NOT NULL,
+  `jabatan` varchar(50) NOT NULL,
+  `jk` varchar(50) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+/*Data for the table `pegawai` */
+
+/*Table structure for table `pekerjaan` */
+
+DROP TABLE IF EXISTS `pekerjaan`;
+
+CREATE TABLE `pekerjaan` (
+  `id_pekerjaan` int(11) NOT NULL AUTO_INCREMENT,
+  `nm_pekerjaan` varchar(50) NOT NULL,
+  PRIMARY KEY (`id_pekerjaan`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=latin1;
+
+/*Data for the table `pekerjaan` */
+
+insert  into `pekerjaan`(`id_pekerjaan`,`nm_pekerjaan`) values (1,'Direktur'),(2,'Sekretaris'),(3,'Bendahara');
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
